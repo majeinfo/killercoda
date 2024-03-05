af0aba6cfba462c56e118f7bacddc23e0d1195ca
jd <root@example.com>
2024-03-05 17:49:57 +0100

    Bug fix

--- pod-error-8/step1/verify.sh
+++ pod-error-8/step1/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPLICAS=$(kubectl get deploy nginx -o jsonpath='{.items[0].status.availableReplicas}')
+REPLICAS=$(kubectl get deploy nginx -o jsonpath='{.status.availableReplicas}')
 if [ "$REPLICAS" != "3" ]; then
 	exit 1
 fi
