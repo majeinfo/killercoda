895d757e8ece34a9994dfea5a99a8cf507ace0a0
jd <root@example.com>
2024-03-06 17:39:18 +0100

    Bug fix

--- 014-svc-error/step1/verify.sh
+++ 014-svc-error/step1/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IP1=$(kubectl get endpoint nginx -o jsonpath='{.subsets[0].addresses[0].ip}')
-IP1=$(kubectl get endpoint nginx -o jsonpath='{.subsets[2].addresses[0].ip}')
+IP1=$(kubectl get endpoints nginx -o jsonpath='{.subsets[0].addresses[0].ip}')
+IP1=$(kubectl get endpoints nginx -o jsonpath='{.subsets[2].addresses[0].ip}')
 
 if [ "$IP1" = "" -o "$IP2" = "" ]; then
 	exit 1
