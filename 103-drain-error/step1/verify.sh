152a0c42b160094cd0d18006dc8994a08d3264e0
jd <root@example.com>
2024-03-07 12:09:26 +0100

    Bug fix

--- 103-drain-error/step1/verify.sh
+++ 103-drain-error/step1/verify.sh
@@ -11,3 +11,13 @@ REPL=$(kubectl get deploy nginx -o jsonpath='{.status.readyReplicas}')
 if [ "$REPL" != "2" ]; then
 	exit 1
 fi
+
+NODE=$(kubectl get po -o jsonpath='{.items[0].spec.nodeName}')
+if [ "$NODE" != "controlplane" ]; then
+	exit 1
+fi
+
+NODE=$(kubectl get po -o jsonpath='{.items[1].spec.nodeName}')
+if [ "$NODE" != "controlplane" ]; then
+	exit 1
+fi
