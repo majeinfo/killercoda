51380977d6734aa70ff17091bcf8c7aa42c27a37
jd <root@example.com>
2024-03-08 11:47:18 +0100

    Bug fix

--- 105-ingress-error/step1/verify.sh
+++ 105-ingress-error/step1/verify.sh
@@ -2,12 +2,12 @@
 
 SVC=$(kubectl get svc ingress-nginx-controller -n ingress-nginx -o jsonpath='{.spec.clusterIP}')
 
-curl -H vote1 http://$SVC | grep "#hostname"
+curl -H "Host: vote1" http://$SVC | grep "#hostname"
 if [ $? -eq 0 ]; then
 	exit 1
 fi
 
-curl -H vote2 http://$SVC | grep "#hostname"
+curl -H "Host: vote2" http://$SVC | grep "#hostname"
 if [ $? -ne 0 ]; then
 	exit 1
 fi
