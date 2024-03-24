0b3289f8e3f8f0fb7881d710acf6593cb8977c18
jd <root@example.com>
2024-03-24 14:00:27 +0100

    Bug fix

--- 017-kubeconfig-error/step1/background.sh
+++ 017-kubeconfig-error/step1/background.sh
@@ -1,5 +1,5 @@
 # Wrong .kube/config file !
 
-sed 's/172\.30\.2\.1:6443/172.31.1.2:443/' -i /root/.kube/config
+sed 's/172\.30\.1\.2:6443/172.31.2.1:443/' -i /root/.kube/config
 
 touch /tmp/finished
